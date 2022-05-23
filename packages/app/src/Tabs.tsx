883d6ca7c354feb3851e4a4e1433650ae02a0680
Eric Horodyski <horodyski@ionic.io>
2022-05-23 12:33:04 -0400

    chore: proof out what's needed to build navigation

--- packages/app/src/Tabs.tsx
+++ packages/app/src/Tabs.tsx
@@ -1,33 +1,73 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { Button, StyleProp, Text, View } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const style: StyleProp = {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const DetailsScreen = () => (
+  <View style={style}>
+    <Text>Details</Text>
+  </View>
+);
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+    <View style={style}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 };
 
 const SettingsScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+    <View style={style}>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 };
+
+const HomeStack = createNativeStackNavigator();
+const HomeStackScreen = () => (
+  <HomeStack.Navigator screenOptions={{ headerLargeTitle: true }}>
+    <HomeStack.Screen name="Home" component={HomeScreen} />
+    <HomeStack.Screen name="Details" component={DetailsScreen} />
+  </HomeStack.Navigator>
+);
+
+const SettingsStack = createNativeStackNavigator();
+const SettingsStackScreen = () => (
+  <SettingsStack.Navigator screenOptions={{ headerLargeTitle: true }}>
+    <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+    <SettingsStack.Screen name="Details" component={DetailsScreen} />
+  </SettingsStack.Navigator>
+);
 
 const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tabs.Navigator>
-        <Tabs.Screen name="Home" component={HomeScreen} />
-        <Tabs.Screen name="Settings" component={SettingsScreen} />
+      <Tabs.Navigator screenOptions={{ headerShown: false }}>
+        <Tabs.Screen name="HomeStack" component={HomeStackScreen} />
+        <Tabs.Screen name="SettingsStack" component={SettingsStackScreen} />
       </Tabs.Navigator>
     </NavigationContainer>
   );
